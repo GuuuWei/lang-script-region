d200086bf355620e0a87eb2036b0915cca6fe401
顾巍 <windknight@vip.qq.com>
2024-10-09 19:43:45 +0800

    sync

--- str2uni_range.py
+++ str2uni_range.py
@@ -1,6 +1,9 @@
-strlist =    "- , . % + 0 1 2 3 4 5 6 7 8 9 〇 一 七 三 九 二 五 八 六 四"
+import re
+strlist = "ء أ ؤ إ ئ ا آ ب ة ت ث ج ح خ د ذ ر ز س ش ص ض ط ظ ع غ ف ق ك ل م ن ه و ى ي ـ  ـ ‌ ‍ ‎ ‏ پ چ ژ ڜ ڢ ڤ ڥ ٯ ڧ ڨ ک گ ی   ◌ٰ ◌ٓ ◌ٔ ◌ٕ ◌ً ◌ٌ ◌ٍ ◌َ ◌ُ ◌ِ ◌ّ ◌ْ  ؜ ‎ - , ٫ ٬ . % ٪ ؉ + 0٠ 1 ١ 2 ٢ 3 ٣ 4 ٤ 5 ٥ 6 ٦ 7 ٧ 8 ٨ 9 ٩   - – — ۔ ، ؛ : ! ؟ ٭ . … \' \" « » ﴾ ﴿ ( ) [ ] ؍   ا ب ت ث ج ح خ د ذ ر ز س ش ص ض ط ظ ع غ ف ق ك ل م ن ه و ي"
 
 
+ss = [x[0] or x[1] for x in re.findall(r"\{([^}]+)\}|(\S+)", strlist)]
+print(ss)
 
 # 去除空格
 strlist = strlist.replace(' ', '')
@@ -12,6 +15,8 @@ cache_list = [ord(c) for c in strlist]
 cache_list = list(set(cache_list))
 # 排序
 cache_list.sort()
+# 移除 9676 ◌
+cache_list.remove(9676)
 
 result = []
 start = cache_list[0]
@@ -23,16 +28,14 @@ for i in range(1, len(cache_list)):
         if start == end:
             result.append([start])
         else:
-            result.append([start ,end])
+            result.append([start, end])
         start = cache_list[i]
         end = cache_list[i]
 if start == end:
     result.append([start])
 else:
-    result.append([start ,end])
+    result.append([start, end])
 print(result)
-
-
 
 
 # 存为txt文件
