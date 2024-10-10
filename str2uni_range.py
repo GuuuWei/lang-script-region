84ac253a425ecbd4eb27c96c75b96ca1167a2a29
顾巍 <windknight@vip.qq.com>
2024-10-10 20:20:36 +0800

    sync

--- str2uni_range.py
+++ str2uni_range.py
@@ -1,5 +1,5 @@
 import re
-strlist = "ء أ ؤ إ ئ ا آ ب ة ت ث ج ح خ د ذ ر ز س ش ص ض ط ظ ع غ ف ق ك ل م ن ه و ى ي ـ  ـ ‌ ‍ ‎ ‏ پ چ ژ ڜ ڢ ڤ ڥ ٯ ڧ ڨ ک گ ی   ◌ٰ ◌ٓ ◌ٔ ◌ٕ ◌ً ◌ٌ ◌ٍ ◌َ ◌ُ ◌ِ ◌ّ ◌ْ  ؜ ‎ - , ٫ ٬ . % ٪ ؉ + 0٠ 1 ١ 2 ٢ 3 ٣ 4 ٤ 5 ٥ 6 ٦ 7 ٧ 8 ٨ 9 ٩   - – — ۔ ، ؛ : ! ؟ ٭ . … \' \" « » ﴾ ﴿ ( ) [ ] ؍   ا ب ت ث ج ح خ د ذ ر ز س ش ص ض ط ظ ع غ ف ق ك ل م ن ه و ي"
+strlist = "- – — , ; : ! ? . … \' ‘ ’ \" “ ” ( ) [ ] @ * / & #"
 
 
 ss = [x[0] or x[1] for x in re.findall(r"\{([^}]+)\}|(\S+)", strlist)]
@@ -15,8 +15,11 @@ cache_list = [ord(c) for c in strlist]
 cache_list = list(set(cache_list))
 # 排序
 cache_list.sort()
-# 移除 9676 ◌
-cache_list.remove(9676)
+
+remove_list = [123, 125, 9676]
+for i in remove_list:
+    if i in cache_list:
+        cache_list.remove(i)
 
 result = []
 start = cache_list[0]
